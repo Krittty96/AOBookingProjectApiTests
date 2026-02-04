76ebf719167f095587acddace1f71625df9731db
Krit <kritkitkit@gmail.com>
2026-02-05 01:55:51 +0700

    исправление ошибок в тестах2

--- tests/test_create_booking.py
+++ tests/test_create_booking.py
@@ -11,74 +11,20 @@ from core.clients.api_client import Endpoints
 @allure.story("Validate response")
 def test_validate_response(api_client, generate_random_booking_data):
     url = f'{api_client.base_url}{Endpoints.BOOKING_ENDPOINT.value}'
-    response = requests.post(url, json=generate_random_booking_data.copy(),
-                             headers={'Content-Type': 'application/json', 'Accept': 'application/json'})
+    booking_data = generate_random_booking_data.copy()
+    response = requests.post(url, json=booking_data)
+    assert response.status_code == 200, f'Expected status code 200 , got {response.status_code}'
     response_data = response.json()
     with allure.step('JSON schema validation'):
         jsonschema.validate(instance=response_data, schema=BOOKING_DETAILS_SCHEMA)
-
-
-
-@allure.feature("Test create booking")
-@allure.story("Test server unavailability")
-def test_server_unavailable(api_client, generate_random_booking_data, mocker):
-    booking_data = generate_random_booking_data.copy()
-    mocker.patch.object(api_client.session, 'post', side_effect=Exception('Server unavailable'))
-    with pytest.raises(Exception, match='Server unavailable'):
-        api_client.create_booking(booking_data)
-
-
-@allure.feature("Test create booking")
-@allure.story("Test wrong HTTP method")
-def test_wrong_method(api_client, generate_random_booking_data, mocker):
-    mock_response = mocker.Mock()
-    booking_data = generate_random_booking_data.copy()
-    mock_response.status_code = 405
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 200 but got 405'):
-        api_client.create_booking(booking_data)
-
-
-@allure.feature("Test create booking")
-@allure.story("Test server error")
-def test_server_error(api_client, generate_random_booking_data, mocker):
-    booking_data = generate_random_booking_data.copy()
-    mock_response = mocker.Mock()
-    mock_response.status_code = 500
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 200 but got 500'):
-        api_client.create_booking(booking_data)
-
-
-@allure.feature("Test create booking")
-@allure.story("Test wrong URL")
-def test_wrong_url(api_client, generate_random_booking_data, mocker):
-    booking_data = generate_random_booking_data.copy()
-    mock_response = mocker.Mock()
-    mock_response.status_code = 404
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 200 but got 404'):
-        api_client.create_booking(booking_data)
-
-
-@allure.feature("Test create booking")
-@allure.story("Test connection with different success code")
-def test_connection_with_different_success(api_client, generate_random_booking_data, mocker):
-    booking_data = generate_random_booking_data.copy()
-    mock_response = mocker.Mock()
-    mock_response.status_code = 404
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-    with pytest.raises(AssertionError, match='Expected status 200 but got 404'):
-        api_client.create_booking(booking_data)
-
-
-@allure.feature("Test create booking")
-@allure.story("Test timeout")
-def test_ping_timeout(api_client, generate_random_booking_data, mocker):
-    booking_data = generate_random_booking_data.copy()
-    mocker.patch.object(api_client.session, 'post', side_effect=requests.Timeout)
-    with pytest.raises(requests.Timeout):
-        api_client.create_booking(booking_data)
+    with allure.step("Verify response fields"):
+        booking_details = response_data['booking']
+        assert booking_details['firstname'] == booking_data['firstname'], f"Firstname mismatch: expected {response_data['firstname']}, got {booking_data['firstname']}"
+        assert booking_details['lastname'] == booking_data['lastname'] , f"Firstname mismatch: expected {response_data['lastname']}, got {booking_data['lastname']}"
+        assert booking_details['totalprice'] == booking_data['totalprice'] , f"Firstname mismatch: expected {response_data['totalprice']}, got {booking_data['totalprice']}"
+        assert booking_details['depositpaid'] == booking_data['depositpaid'], f"Firstname mismatch: expected {response_data['depositpaid']}, got {booking_data['depositpaid']}"
+        assert booking_details['bookingdates'] == booking_data['bookingdates'] , f"Firstname mismatch: expected {response_data['bookingdates']}, got {booking_data['bookingdates']}"
+        assert booking_details['additionalneeds'] == booking_data["additionalneeds"], f"Firstname mismatch: expected {response_data['additionalneeds']}, got {booking_data['additionalneeds']}"
 
 
 @allure.feature("Test create booking")
